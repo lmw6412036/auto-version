4f9fb30a250a01798dc0a7939173796523c03f40
TY-089\Administrator <lmw6412036@sina.com>
2017-06-15 15:09:51 +0800

    修改

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 /**
  * Created by Administrator on 2017/6/15 0015.
  */
+let autoVersion = require("./main");
+let path = require("path");
 let co = require('hprose').co;
-let thunkify = require("thunkify");
-let fs = require("fs");
+//co(autoVersion());
 
-/*co(function *() {
-    let file = "./package.json";
-    let pkg = yield thunkify(fs.readFile)(file, "utf-8")
-    pkg = JSON.parse(pkg);
-    let arr = pkg.version.split(".");
-    arr[arr.length - 1]++;
-    pkg.version = arr.join(".");
-    yield thunkify(fs.writeFile)(file, JSON.stringify(pkg, (key, val) => {
-        return val;
-    }, 4));
-})*/
-
-export default function (file) {
-    co(function *() {
-        let pkg = yield thunkify(fs.readFile)(file, "utf-8")
-        pkg = JSON.parse(pkg);
-        let arr = pkg.version.split(".");
-        arr[arr.length - 1]++;
-        pkg.version = arr.join(".");
-        yield thunkify(fs.writeFile)(file, JSON.stringify(pkg, (key, val) => {
-            return val;
-        }, 4));
-    })
-}
-
+let file = path.join(__dirname, "./package.json");
+co(autoVersion(file));
 
---
+++ main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 2017/6/15 0015.
+ */
+/**
+ * Created by Administrator on 2017/6/15 0015.
+ */
+let co = require('hprose').co;
+let thunkify = require("thunkify");
+let fs = require("fs");
+
+/*co(function *() {
+ let file = "./package.json";
+ let pkg = yield thunkify(fs.readFile)(file, "utf-8")
+ pkg = JSON.parse(pkg);
+ let arr = pkg.version.split(".");
+ arr[arr.length - 1]++;
+ pkg.version = arr.join(".");
+ yield thunkify(fs.writeFile)(file, JSON.stringify(pkg, (key, val) => {
+ return val;
+ }, 4));
+ })*/
+
+module.exports = function* (file) {
+    let pkg = yield thunkify(fs.readFile)(file, "utf-8")
+    pkg = JSON.parse(pkg);
+    let arr = pkg.version.split(".");
+    arr[arr.length - 1]++;
+    pkg.version = arr.join(".");
+    yield thunkify(fs.writeFile)(file, JSON.stringify(pkg, (key, val) => {
+        return val;
+    }, 4));
+}
+
+
